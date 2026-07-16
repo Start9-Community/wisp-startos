Start9-Community/vikunja-startos

chore: migrate to start-sdk 2.0
Co-Authored-By: Claude Fable 5 <noreply@anthropic.com>
Claude-Session: https://claude.ai/code/session_01F5qZh4QiVySBLkV5ya7XSr

startos/interfaces.ts
@@ -1,15 +1,15 @@
 import { i18n } from './i18n'
 import { sdk } from './sdk'
-import { uiPort } from './utils'
+import { mainHostId, uiPort, webuiInterfaceId } from './utils'
 
 export const setInterfaces = sdk.setupInterfaces(async ({ effects }) => {
-  const multi = sdk.MultiHost.of(effects, 'main')
+  const multi = sdk.MultiHost.of(effects, mainHostId)
   const origin = await multi.bindPort(uiPort, {
     protocol: 'http',
   })
   const webui = sdk.createInterface(effects, {
     name: i18n('Web UI'),
-    id: 'webui',
+    id: webuiInterfaceId,
     description: i18n('The Vikunja web interface'),
     type: 'ui',
     masked: false,

startos/main.ts
@@ -39,7 +39,7 @@ export const main = sdk.setupMain(async ({ effects }) => {
     )
   }
 
-  const sub = await sdk.SubContainer.of(
+  const sub = sdk.SubContainer.of(
     effects,
     { imageId: 'vikunja' },
     dataMount,

Start9-Community/wisp-startos

startos/interfaces.ts
@@ -1,9 +1,9 @@
 import { i18n } from './i18n'
 import { sdk } from './sdk'
-import { relayInterfaceId, relayPort } from './utils'
+import { relayInterfaceId, relayPort, relayHostId } from './utils'
 
 export const setInterfaces = sdk.setupInterfaces(async ({ effects }) => {
-  const wsMulti = sdk.MultiHost.of(effects, 'websocket')
+  const wsMulti = sdk.MultiHost.of(effects, relayHostId)
   const wsOrigin = await wsMulti.bindPort(relayPort, {
     protocol: 'ws',
   })

startos/main.ts
@@ -20,7 +20,7 @@ export const main = sdk.setupMain(async ({ effects }) => {
       readonly: true,
     })
 
-  const subcontainer = await sdk.SubContainer.of(
+  const subcontainer = sdk.SubContainer.of(
     effects,
     { imageId: 'wisp' },
     mounts,