Start9-Community/isso-startos

fix: point packageRepo at Start9-Community fork

startos/manifest/index.ts
@@ -10,7 +10,7 @@ export const manifest = setupManifest({
   id: 'isso',
   title: 'Isso',
   license: 'MIT',
-  packageRepo: 'https://github.com/kwsantiago/isso-startos',
+  packageRepo: 'https://github.com/Start9-Community/isso-startos',
   upstreamRepo: 'https://github.com/isso-comments/isso',
   marketingUrl: 'https://isso-comments.de/',
   donationUrl: null,

Start9-Community/wisp-startos

startos/manifest/index.ts
@@ -6,7 +6,7 @@ export const manifest = setupManifest({
   title: 'Wisp',
   license: 'MIT',
   donationUrl: null,
-  packageRepo: 'https://github.com/privkeyio/wisp-startos',
+  packageRepo: 'https://github.com/Start9-Community/wisp-startos',
   upstreamRepo: 'https://github.com/privkeyio/wisp',
   marketingUrl: 'https://github.com/privkeyio/wisp',
   description: { short, long },